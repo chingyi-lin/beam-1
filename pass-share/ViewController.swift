a20c4db1909405885d04fa59fbb6c0f1ccf087c1
chingyi-lin <icequ60914@gmail.com>
2019-03-17 17:32:13 -0700

    init xcode proj

---
+++ pass-share/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pass-share
+//
+//  Created by Ching-Yi Lin on 2019/3/17.
+//  Copyright © 2019 Pass Share. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
